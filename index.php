74759a176f76e7b6da1f3582168564e92e930b3b
cpb-work <cpb@bizb.ru>
2024-09-19 12:27:35 +0300

    1

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+echo "Введите первое целое число: ";
+$num1 = trim(fgets(STDIN));
+
+while (!(int)$num1) {
+    echo "Введённое не является целым числом. Введите целое число: ";
+    $num1 = trim(fgets(STDIN));
+}
+
+echo "Введите ещё одно целое число: ";
+$num2 = trim(fgets(STDIN));
+
+while (!(int)$num2) {
+    if ($num2 == 0) {
+        fwrite(STDERR, "Делить на 0 нельзя");
+        exit;
+    } else {
+        echo "Введённое не является целым числом. Введите целое число: ";
+        $num2 = trim(fgets(STDIN));
+    }
+}
+
+
+    fwrite(STDOUT, "Результат: $num1 / $num2 = " . $num1 / $num2);
