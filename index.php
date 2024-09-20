4bac4a9bede249a7d93abeb7cbca76b9e0c9755a
cpb-work <cpb@bizb.ru>
2024-09-20 09:16:07 +0300

    changed !(int)$num1 to !is_numeric($num2)

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 echo "Введите первое целое число: ";
 $num1 = trim(fgets(STDIN));
 
-while (!(int)$num1) {
+while (!is_numeric($num1)) {
     echo "Введённое не является целым числом. Введите целое число: ";
     $num1 = trim(fgets(STDIN));
 }
@@ -10,14 +10,13 @@ while (!(int)$num1) {
 echo "Введите ещё одно целое число: ";
 $num2 = trim(fgets(STDIN));
 
-while (!(int)$num2) {
+while (!is_numeric($num2) OR $num2 == 0) {
     if ($num2 == 0) {
-        fwrite(STDERR, "Делить на 0 нельзя");
-        exit;
+        fwrite(STDERR, "Делить на 0 нельзя. Введите другое целое число: ");
     } else {
         echo "Введённое не является целым числом. Введите целое число: ";
-        $num2 = trim(fgets(STDIN));
     }
+    $num2 = trim(fgets(STDIN));
 }
 
 fwrite(STDOUT, "Результат: $num1 / $num2 = " . $num1 / $num2);
